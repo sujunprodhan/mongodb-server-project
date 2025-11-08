76f8fd78b5b8b908e6f46cc2c05cd5a18c75b538
sujunprodhan <144489247+sujunprodhan@users.noreply.github.com>
2025-11-09 01:24:40 +0600

    mongodb database conection and json added

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const cors = require('cors');
+const port = 3000;
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cruduser.c6e8c0q.mongodb.net/?appName=Cruduser`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db('smartband')
+    const productCollection = db.collection('realagent')
+
+
+    // get api and all product find and findOne
+
+    app.get('/realagent', async(req, res)=>{
+     const result = await productCollection?.find()?.toArray();
+      res.send(result)
+    })
+
+
+
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+  } finally {
+    
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
