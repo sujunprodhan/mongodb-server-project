17b8c1a177b49f50adc65c7dd58f51d9671496b1
sujunprodhan <144489247+sujunprodhan@users.noreply.github.com>
2025-11-10 22:22:43 +0600

    get id for property collection

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const port = 3000;
 require('dotenv').config();
 
 app.use(cors());
-app.use(express.json())
-
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cruduser.c6e8c0q.mongodb.net/?appName=Cruduser`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -22,28 +20,32 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const db = client.db('smartband')
-    const productCollection = db.collection('realagent')
-
+    const db = client.db('smartband');
+    const propertyCollection = db.collection('realagent');
 
     // get api and all product find and findOne
 
-    app.get('/realagent', async(req, res)=>{
-     const result = await productCollection?.find()?.toArray();
-      res.send(result)
-    })
+    app.get('/realagent', async (req, res) => {
+      const result = await propertyCollection?.find()?.toArray();
+      res.send(result);
+    });
 
-
+    // post methord
+    // insert, insertOne
+    app.get('/realagent/:id', async (req, res) => {
+      const id = req.params.id;
+      const quary = new ObjectId(id);
+      const result = await propertyCollection.findOne({_id: quary})
+      res.send(result);
+    });
 
     await client.db('admin').command({ ping: 1 });
     console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } finally {
-    
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
